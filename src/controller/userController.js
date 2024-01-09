0823020bcf7d609da575e2af842f7313d82c64af
Batisco <bluespicemerchant@gmail.com>
2024-01-09 14:23:42 +0600

    Определен интерфейс контроллера

---
+++ src/controller/userController.js
@@ -0,0 +1,32 @@
+import newLogger from '../conf/logConf.js';
+
+const logger = newLogger('info', 'userController.js', 'AuthServise');
+
+class UserController {
+
+    constructor() {
+
+    }
+
+    async enter(req, res, next) {
+        logger.info(`user with email '%s' try enter`, req.body.email);
+        res.status(200).send('ok');
+    }
+
+    async registration(req, res, next) {
+        logger.info(`user '%s' try register`, { firstName: req.body.firstName, email: req.body.email });
+        res.status(200).send('ok');
+    }
+
+    async getById(req, res, next) {
+        logger.info(`get user by id=%s`, req.params.id);
+        res.status(200).send('ok');
+    }
+
+    async update(req, res, next) {
+        logger.info(`update user by id=%s with data=%s`, req.params.id, req.body);
+        res.status(200).send('ok');
+    }
+};
+
+export default UserController;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDoc from '../public/swagger.json' assert { type: "json" };
 import newLogger from './conf/logConf.js';
+import UserController from './controller/userController.js';
 
 const logger = newLogger('info', 'index.js', 'AuthServise');
+const userController = new UserController();
 const app = express();
 
 app.use(express.json());
 app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.post('/user/register', wrapAsync(userController.registration, userController));
+app.post('/user/loggin', wrapAsync(userController.enter, userController));
+app.get('/profile/:id', wrapAsync(userController.getById, userController));
+app.put('/profile/:id', wrapAsync(userController.update, userController));
 app.listen(8080, () => logger.info('Server was started successfully.'));
 
 
-function wrapAsync(callback) {
+function wrapAsync(callback, context) {
+    callback = callback.bind(context);
     return function(req, res, next) {
         return callback(req, res, next).catch(err => next(err));
     }
